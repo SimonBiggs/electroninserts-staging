60b1528d7d80afb6ca3e2c9c4e73b6d94b5931e8
SimonBiggs <mail@simonbiggs.net>
2017-06-05 15:51:02 +1000

    Auto-generated commit

---
+++ assets/run_webapp_locally.py
@@ -0,0 +1,39 @@
+import os
+from subprocess import call
+from multiprocessing import Process
+
+import http.server
+import socketserver
+
+
+def run_api_server():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    call([
+        "python", os.path.join(dir_path, 
+        'server', 'electronfactor-server', 'main.py')])
+
+
+def run_http_server():
+    PORT = 8000
+    Handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", PORT), Handler)
+    httpd.serve_forever()
+
+
+print("==============================================")
+print("Make sure your use the following addresses for your parameterisation and model server:")
+print("    http://localhost:5000/parameterise")
+print("    http://localhost:5000/model")
+print("==============================================")
+print("")
+print("==============================================")
+print("Starting parameterisation and modelling server")
+Process(target=run_api_server).start()
+print("Parameterisation and modelling server is now running at http://localhost:5000")
+print("==============================================")
+print("")
+print("==============================================")
+print("Starting http server")
+Process(target=run_http_server).start()
+print("Http server started. Type http://localhost:8000 within a browser to access the webapp.")
+print("==============================================")
